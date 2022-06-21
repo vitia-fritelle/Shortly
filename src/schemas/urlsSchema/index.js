01ea39b4816b01c997f0b2b7f3e1118d3b8d67d9
Mateus Brasil <Mateus-code-98>
2022-06-21 11:53:42 -0300

    Preparando revisão de código

--- src/schemas/urlsSchema/index.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const urlsSchema = Joi.object({
-    url: Joi.string().regex(/^https:\/\/|http:\/\//),
-});
-
-export default urlsSchema;
--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import CustomError from './CustomError';
-import joiErrorMessage from './joiErrorMessage';
-
-const utils = {
-    CustomError,
-    joiErrorMessage,
-};
-
-export default utils;
